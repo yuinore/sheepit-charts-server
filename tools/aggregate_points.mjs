ac61e103271fe5c26d93c1135fe7d3c2707142e7
yuinore <yuinore@hotmail.co.jp>
2022-02-15 05:31:13 -0800

    pointsグラフ調整

--- tools/aggregate_points.mjs
+++ tools/aggregate_points.mjs
@@ -18,19 +18,36 @@ const { JSDOM } = jsdom;
       );
     });
 
+    const labels = [];
     const points = [];
 
     for (const filename of fileList) {
       const filebasename = filename.match(/(.*)\.json/)[1];
+      const dateStr = filebasename.split('_')[1];
+      const timeStr = filebasename.split('_')[2];
+      const datetime =
+        `${dateStr.substr(0, 4)}-` +
+        `${dateStr.substr(4, 2)}-` +
+        `${dateStr.substr(6, 2)}T` +
+        `${timeStr.substr(0, 2)}:` +
+        `${timeStr.substr(2, 2)}:` +
+        `${timeStr.substr(4, 2)}Z`;
+      console.log(datetime);
+      const date = new Date(datetime);
+      console.log(date);
+
       const data = fs.readFileSync(`./log/profile/${filebasename}.json`);
       const column = JSON.parse(data);
       if (column.length >= 1 && column[0] != -1) {
+        labels.push(datetime);
         points.push(column[0]);
       }
     }
 
     let str = '';
+    str += `labels = ${JSON.stringify(labels)}; `;
     str += `points = ${JSON.stringify(points)}; `;
+    console.log(`labels = ${JSON.stringify(labels)}`);
     console.log(`points = ${JSON.stringify(points)}`);
 
     fs.writeFileSync(`./visualizer/data/profile.js`, str);
